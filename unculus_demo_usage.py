a4a5da5aa047303747e554e9020190253379a8cc
Erik Pohl <epohl@dmigs.com>
2024-11-23 19:07:13 -0500

    cleaned the code a bit for better outputs

--- unculus_demo_usage.py
+++ unculus_demo_usage.py
@@ -1,14 +1,14 @@
 from unculus_node import unculus_node
 
 
-def foo(z):
-    print(f'doing something with value {str(z)}')
-    print(str(z+zappo))
+def foo(z_func_arg):
+    print(f"doing something with value {str(z_func_arg)}")
+    print(f"adding zappo to z and making a string {str(z_func_arg + zappo)}")
 
 
-def bar(z):
-    print('doing something with entrance token {str(z)}')
-    print(str(z+zappo))
+def bar(z_func_arg):
+    print(f'doing something with entrance token {str(z_func_arg)}')
+    print(f"adding zappo to z and making a string {str(z_func_arg + zappo)}")
 
 
 zappo = 2
@@ -45,5 +45,4 @@ print(head.is_consumed_completing_sequence(z))
 
 print("single evaluation test here -------------------------")
 nextone = head.evaluate_token(2)
-print("moved to")
-print(nextone.value)
+print(f"moved to {nextone.value}")
